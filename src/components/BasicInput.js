17c0dd025ee7a95c8afd672394e0b37fd7649f75
Pratikesh-kumar <pratikeshkumar96@gmail.com>
2023-05-11 23:41:39 +0530

    added basic validation(useState) and the input color changes

---
+++ src/components/BasicInput.js
@@ -0,0 +1,45 @@
+
+import { useState } from 'react';
+
+const BasicInput = (props) => {
+  const [inputValue, setInputValue] = useState('');
+  const [inputIsValid, setInputIsValid] = useState(true);
+
+  const formSubmitHandler = (event) => {
+    //stop refreshing webpage- default browser behavior
+    event.preventDefault();
+
+    if (inputValue.trim() === '') {
+      setInputIsValid(false);
+      return;
+    }
+    setInputIsValid(true);
+    console.log(inputValue);
+  };
+
+  const inputChangeHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const inputClasses = inputIsValid ? 'form-control' : 'form-control invalid';
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={inputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input
+          type='text'
+          id='name'
+          autoComplete='off'
+          onChange={inputChangeHandler}
+          value={inputValue}
+        />
+        {!inputIsValid && <p className='error-text'>Input is invalid!</p>}
+      </div>
+      <div className='form-actions'>
+        <button type='submit'>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default BasicInput;
